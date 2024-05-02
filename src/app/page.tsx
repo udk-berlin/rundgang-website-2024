84556acbd986181b55ab78ac5e9d702a72aee829
marisanest <marisanest@mailbox.org>
2024-05-02 22:19:20 +0200

    Lint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
-import LandingPageComponent from './LandingPage';
+import Landing from './components/landing.server';
 
-export default function Home() {
+export default function LandingPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <LandingPageComponent />
+        <Landing />
       </div>
     </main>
   );
