1e720d93578091e1b74d7630885710a3d7da10ce
lillijo <lillijoppien@gmail.com>
2024-06-20 23:43:16 +0200

    fix link in link error

--- src/components/filterTag.tsx
+++ src/components/filterTag.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { useCallback } from 'react';
 import Link from 'next/link';
 import { useSearchParams, useSelectedLayoutSegment } from 'next/navigation';
 import { Filter } from '@/types/types';
 import cx from 'classnames';
+import useQuery from '@/lib/useQuery';
 
 export type FilterTagProps = {
   filter: Filter;
@@ -17,29 +19,19 @@ export default function FilterTag({
   isReverse = false,
   disabled = false,
 }: FilterTagProps) {
-  const selectedLayoutSegment = useSelectedLayoutSegment(); // todo: why are we using this instead of usePathname() ?
-  const searchParams = useSearchParams();
+  const { changeParameter } = useQuery(filter.searchParam);
+
+  const handleClickFilter = useCallback(() => {
+    changeParameter(filter?.id);
+  }, [filter?.id, changeParameter]);
 
   return (
-    <Link
-      href={{
-        pathname: selectedLayoutSegment,
-        query: {
-          // this should be wrapped nicely in some utility function or so?
-          ...(filter.searchParam != 'language' && searchParams.get('language')
-            ? { language: searchParams.get('language') }
-            : {}),
-          ...(filter.searchParam != 'faculty' && searchParams.get('faculty')
-            ? { faculty: searchParams.get('faculty') }
-            : {}),
-          ...(filter.searchParam != 'format' && searchParams.get('format')
-            ? { format: searchParams.get('format') }
-            : {}),
-          ...(isSelected ? {} : { [filter.searchParam]: filter?.id }),
-          //[filter.searchParam]: filter?.id,
-        },
-      }}
-      className={cx('pl-1 pt-1', disabled && 'pointer-events-none')}
+    <div
+      onClick={handleClickFilter}
+      className={cx(
+        'cursor-pointer pl-1 pt-1',
+        disabled && 'pointer-events-none',
+      )}
     >
       <div
         className={cx(
@@ -52,6 +44,6 @@ export default function FilterTag({
       >
         {filter.name}
       </div>
-    </Link>
+    </div>
   );
 }
