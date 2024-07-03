a676328f448cd60146d9dca67e6e2e5f2fc37a3c
lillijo <lillijoppien@gmail.com>
2024-07-03 20:10:39 +0200

    fix overflow timeline

--- src/app/[locale]/timeline/[[...slug]]/components/timetable.server.tsx
+++ src/app/[locale]/timeline/[[...slug]]/components/timetable.server.tsx
@@ -11,20 +11,22 @@ export default async function TimeTable() {
   const items = await getEventLocations();
 
   return (
-    <div
-      className="relative h-fit w-screen overflow-scroll"
-      id="timetable-container"
-    >
-      <TimelineDays />
-      <TimeScale />
-      <TimeGrid />
-      {items.map((eventLocation) => (
-        <EventContainer key={eventLocation.id} location={eventLocation}>
-          <EventLocation location={eventLocation} />
-        </EventContainer>
-      ))}
-      <ScrollForwardButton />
-      <ScrollBackwardButton />
+    <div className="relative h-content w-full overflow-x-visible overflow-y-scroll">
+      <div
+        className="relative h-fit w-screen overflow-y-clip overflow-x-scroll"
+        id="timetable-container"
+      >
+        <TimelineDays />
+        <TimeScale />
+        <TimeGrid />
+        {items.map((eventLocation) => (
+          <EventContainer key={eventLocation.id} location={eventLocation}>
+            <EventLocation location={eventLocation} />
+          </EventContainer>
+        ))}
+        <ScrollForwardButton />
+        <ScrollBackwardButton />
+      </div>
     </div>
   );
 }
