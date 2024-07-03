cbb504b570700952587e620c8c6dbfa011ebb25c
lillijo <lillijoppien@gmail.com>
2024-07-03 18:43:58 +0200

    only play audio once on landing

--- src/app/[locale]/(landing)/components/desktop/info/project/audioPlayer.client.tsx
+++ src/app/[locale]/(landing)/components/desktop/info/project/audioPlayer.client.tsx
@@ -1,17 +1,49 @@
 'use client';
-import AudioPlayer from '@/components/audioPlayer';
-import { LandingInfoProps } from '@/app/(landing)/components/info/info.server';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslations } from 'next-intl';
+import Unmute from '@/components/icons/unmute';
+import Mute from '@/components/icons/mute';
+import { LandingInfoProps } from '../info.server';
 
 export default function LandingInfoProjectAudioPlayer({
   language,
 }: LandingInfoProps) {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [muted, setIsMuted] = useState(true);
+
+  const setPlay = useCallback(() => {
+    setIsMuted(false);
+    if (audioRef?.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
+  }, [audioRef]);
+
+  const onEnded = useCallback(() => {
+    setIsMuted(true);
+    if (audioRef?.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  }, [audioRef]);
+
+  const t = useTranslations('errors');
+
   return (
     <div className="ml-auto">
-      <AudioPlayer
-        src={`/assets/projects/${language}/audio.mp3`}
-        width={25}
-        height={25}
-      />
+      <div className="w-full">
+        <audio ref={audioRef} onEnded={onEnded}>
+          <source
+            src={`/assets/projects/${language}/audio.mp3`}
+            type="audio/mpeg"
+          />
+          {t('audiPlayer')}
+        </audio>
+        <div className="h-auto w-full cursor-pointer" onClick={setPlay}>
+          {!muted && <Unmute width={25} height={25} />}
+          {muted && <Mute width={25} height={25} />}
+        </div>
+      </div>
     </div>
   );
 }
