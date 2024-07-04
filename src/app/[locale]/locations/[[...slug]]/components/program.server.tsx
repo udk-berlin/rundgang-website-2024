10b30f2ef7559c5a3098a3535910c5a7b69a4775
lillijo <lillijoppien@gmail.com>
2024-07-04 20:13:01 +0200

    little fix on locations program

--- src/app/[locale]/locations/[[...slug]]/components/program.server.tsx
+++ src/app/[locale]/locations/[[...slug]]/components/program.server.tsx
@@ -7,12 +7,16 @@ import { Suspense } from 'react';
 
 export type ProgramPageProps = {
   place: string;
+  searchParams: { [key: string]: string | undefined };
 };
 
 export const revalidate = 3600;
 
-export default async function ProgramPage({ place }: ProgramPageProps) {
-  const items = await getFilteredGraphQLLocationItems(place);
+export default async function ProgramPage({
+  place,
+  searchParams,
+}: ProgramPageProps) {
+  const items = await getFilteredGraphQLLocationItems(place, searchParams);
 
   return (
     items && (
@@ -21,6 +25,7 @@ export default async function ProgramPage({ place }: ProgramPageProps) {
           <ProjectCard
             key={item.id}
             item={item}
+            contextTagType="plain"
             linkPathname={`/locations/${place}`}
           />
         ))}
@@ -34,9 +39,9 @@ export type ProgramProps = {
 
 function ProgramContainer({ children }: ReactNodeProps) {
   return (
-    <div className="z-40 col-span-1 -ml-xs h-content max-h-content min-h-content w-full bg-primary md:col-span-2 md:overflow-y-scroll">
+    <div className="z-40 col-span-1 -ml-xs flex h-content max-h-content min-h-content w-full bg-primary md:col-span-2 md:overflow-y-scroll">
       <Suspense>
-        <div className="col-span-1 grid w-full columns-1 grid-cols-2 items-start justify-start gap-border bg-primary px-border">
+        <div className="wrap grid w-full grid-cols-2 justify-stretch gap-border bg-primary px-border">
           {children}
         </div>
       </Suspense>
