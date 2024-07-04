841b7e3f4b450c9d6bdff60125dd58a708675915
lillijo <lillijoppien@gmail.com>
2024-07-04 16:11:48 +0200

    add server side intl fallback

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,28 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
+import { IntlErrorCode } from 'next-intl';
 
 // Can be imported from a shared config
 export const locales = ['en', 'de'];
 
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+export default getRequestConfig(async ({ locale }) =>
+  !locales.includes(locale as any)
+    ? notFound()
+    : {
+        onError(error) {
+          if (error.code !== IntlErrorCode.MISSING_MESSAGE) {
+            console.log(error);
+          }
+        },
+        getMessageFallback({ namespace, key, error }) {
+          return key;
+        },
 
-  return {
-    messages: (
-      await (locale === 'en'
-        ? // When using Turbopack, this will enable HMR for `en`
-          import('../locales/en.json')
-        : import(`../locales/${locale}.json`))
-    ).default,
-  };
-});
+        messages: (
+          await (locale === 'en'
+            ? // When using Turbopack, this will enable HMR for `en`
+              import('../locales/en.json')
+            : import(`../locales/${locale}.json`))
+        ).default,
+      },
+);
